2b77a7a944077c06b726017d4f41c79b40106e46
PCNRepository <phamchinam1998@gmail.com>
2021-09-22 01:10:42 +0700

    fix request

--- src/component/navigation/cart/modifycart.js
+++ src/component/navigation/cart/modifycart.js
@@ -13,14 +13,15 @@ export function AddCart(item_id, token, authen) {
     else window.localStorage.setItem('PCN-Cart', `${item_id}`);
     if (authen === "Allow") {
         axios.post(`https://demo-ecomerce-backend.herokuapp.com/authorization/cart/additem`, {
-            token: token,
             item_id: item_id,
+        }, {
+            headers: {
+                Authorization: token
+            }
         }).then(function (response) {
-            // handle success
             console.log(response);
         })
             .catch(function (error) {
-                // handle error
                 console.log(error);
             })
             .then(function () {
@@ -36,14 +37,15 @@ export function DeleteCartItem(id, token, authen) {
     window.localStorage.setItem('PCN-Cart', arr.join('&'));
     if (authen === "Allow") {
         axios.post(`https://demo-ecomerce-backend.herokuapp.com/authorization/cart/deleteitem`, {
-            token: token,
             item_id: id,
+        }, {
+            headers: {
+                Authorization: token
+            }
         }).then(function (response) {
-            // handle success
             console.log(response);
         })
-            .catch(function (error) {      // DONE
-                // handle error
+            .catch(function (error) {
                 console.log(error);
             })
             .then(function () {
@@ -55,13 +57,14 @@ export function DeleteCart(token, authen) {
     window.localStorage.removeItem('PCN-Cart');
     if (authen === "Allow") {
         axios.post(`https://demo-ecomerce-backend.herokuapp.com/authorization/cart/deleteall`, {
-            token: token,
+        }, {
+            headers: {
+                Authorization: token
+            }
         }).then(function (response) {
-            // handle success
             console.log(response);
         })
-            .catch(function (error) {          //DONE
-                // handle error
+            .catch(function (error) {
                 console.log(error);
             })
             .then(function () {
